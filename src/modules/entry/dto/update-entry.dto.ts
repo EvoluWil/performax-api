b92799ed6f9256a9434eff57e4a25325adb093ad
Willian Rodrigues <willian.rsilva1994@gmail.com>
2024-09-22 16:34:18 -0300

    fix approved entry bug

--- src/modules/entry/dto/update-entry.dto.ts
+++ src/modules/entry/dto/update-entry.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { EntryStatusEnum } from '@prisma/client';
-import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { IsBoolean, IsEnum, IsOptional, IsString } from 'class-validator';
 import { CreateEntryDto } from './create-entry.dto';
 
 export class UpdateEntryDto extends PartialType(CreateEntryDto) {
@@ -8,4 +8,8 @@ export class UpdateEntryDto extends PartialType(CreateEntryDto) {
   @IsOptional()
   @IsEnum(EntryStatusEnum)
   status: EntryStatusEnum;
+
+  @IsBoolean()
+  @IsOptional()
+  approved: boolean;
 }
